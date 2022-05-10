283fed2362281e7d2e60397e08a470baec1753bb
keiner <tim.keiner@extern.brz.gv.at>
2022-05-10 19:14:28 +0200

    fixing ts error

--- src/App.vue
+++ src/App.vue
@@ -55,12 +55,11 @@ const emojis= {
       <ul class="flex gap-4 items-center">
         <li v-for="(q, i) of questions" :key="q.question" class="transform block transition-transform relative"
           :class="{ 'scale-150': current === i }">
-          <button class="relative rounded-full text-white h-12 w-12 overflow-hidden flex items-center"
-            @click.prevent.stop="changeQuestion(i)">
+          <div class="relative rounded-full text-white h-12 w-12 overflow-hidden flex items-center">
             <img v-if="q.author.avatar" :src="q.author.avatar" :class="{'grayscale': current > i}" />
             <div v-else class="bg-blue-500 w-full h-full text-xl flex items-center justify-center">{{ q.author.name[0] }}
             </div>
-          </button>
+          </div>
           <svg v-if="current > i && solutionAnswered[i] !==codeWrong" class="absolute bottom-0 right-0 text-lime-500 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
           <svg v-if="current > i && solutionAnswered[i] ===codeWrong" class="absolute bottom-0 right-0 text-red-500 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
         </li>
